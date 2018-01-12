56b81aa00a876b09b10749223125eb4a42d786f3
Asgedom1 <35047289+Asgedom1@users.noreply.github.com>
2018-01-11 18:34:51 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ var firstName;
 var lastName;
 var yearsOfStudy;
 var goal;
+<html>
+<body>
+<h2>Assignment 1</h2>
+<script src="code.js"></script>
+</body
+</html>
+
+<html>
+<body>
+<h2>Assignment 1</h2>
+<script>
+ // this is a comment
+ var x = 5;
+ var firstname = "ted";
+ document.write("my name is " + firstname);
+</script>
+</body
+</html>
