f92ac311fe2ac7b6f6dfcc7e844379ed0593ed57
Asgedom1 <35047289+Asgedom1@users.noreply.github.com>
2018-01-11 19:13:45 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var lastName="Hailemariam";
 var yearsOfStudy="2 year";
 var goal="web developer";
 var firstname = "Asgedom";
-document.write("my name is " + firstname lastname +".I hanve been at SCC for"+yearsOfStudy + "and I want to be a "+goal+"after finising the school"); );
+document.write("my name is " + firstname lastName +".I hanve been at SCC for"+yearsOfStudy + "and I want to be a "+ goal + "after finising the school"); );
 
 
 
